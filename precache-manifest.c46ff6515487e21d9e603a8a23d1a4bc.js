f0ddb7c754d9fafc117909f44e750b3446539f87
Cyril M <marceau.cyril@hotmail.fr>
2020-10-25 23:15:08 +0100

    Updates

---
+++ precache-manifest.c46ff6515487e21d9e603a8a23d1a4bc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df669c32f036d108f4e4d3bc7e7b106f",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "37a7d690b35799ec025e",
+    "url": "/build/static/css/main.b8c1891f.chunk.css"
+  },
+  {
+    "revision": "1d4167a340ab1e5f83a9",
+    "url": "/build/static/js/2.40802cee.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/build/static/js/2.40802cee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a7d690b35799ec025e",
+    "url": "/build/static/js/main.a2ad8ce7.chunk.js"
+  },
+  {
+    "revision": "826e05e24cb1895dda31",
+    "url": "/build/static/js/runtime-main.5b96c6fe.js"
+  },
+  {
+    "revision": "02b7d5d2ce12b058070f3958b18fc46c",
+    "url": "/build/static/media/Account.02b7d5d2.png"
+  },
+  {
+    "revision": "1b27b6cc80da28507fd4a8ef0310ec15",
+    "url": "/build/static/media/LoginSignup.1b27b6cc.png"
+  },
+  {
+    "revision": "d5e2c43825d91e6fc3c507c9c16aeca2",
+    "url": "/build/static/media/Payment.d5e2c438.png"
+  },
+  {
+    "revision": "9b5c82e45f4e4d55e85287ae95a54c92",
+    "url": "/build/static/media/subset-Montserrat-Bold.9b5c82e4.woff2"
+  },
+  {
+    "revision": "b78f0adaacf8cafdb2b70c760f4b9897",
+    "url": "/build/static/media/subset-Montserrat-Bold.b78f0ada.woff"
+  },
+  {
+    "revision": "a83a42c04231821f8786bdba011bd3a1",
+    "url": "/build/static/media/subset-Montserrat-Regular.a83a42c0.woff2"
+  },
+  {
+    "revision": "faa13ea75671b2f37df1548fb3fbbe30",
+    "url": "/build/static/media/subset-Montserrat-Regular.faa13ea7.woff"
+  }
+]);
